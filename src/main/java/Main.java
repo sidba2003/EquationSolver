864cc4db18b128a90c57e363a57eb97599ba036d
Siddharth Bahl <98782166+sidba2003@users.noreply.github.com>
2023-08-21 19:42:18 +0100

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,13 +6,16 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
+        /** Enter the equation in the form of "input:x=<value>;y=<equation>" **/
         System.out.println("Enter equation");
         String equation = scanner.nextLine();
 
         System.out.println("Equation is: " + equation);
-
+        
+        /** we get the convert the quation to a json type which is then sent on to the InputSolver class **/
         InputSolver ans = new InputSolver(InputHandler.generateInput(equation));
-
+        
+        /** the computed value is returned in the form of a string using a get method **/
         System.out.println(ans.getSolution());
     }
 }
